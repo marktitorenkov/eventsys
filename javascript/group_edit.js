8dea89ed2d0ba4ad4157f4fc977f3e406383f1c5
RadoslavBotov <92144419+RadoslavBotov@users.noreply.github.com>
2025-01-12 17:22:11 +0200

    normalize EOL to LF

--- javascript/group_edit.js
+++ javascript/group_edit.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // get resources
-    let deleteGroupBtn = document.getElementById('btn-delete-group');
+  // get resources
+  let deleteGroupBtn = document.getElementById('btn-delete-group');
 
-    // add confirmation popup
-    deleteGroupBtn.addEventListener('click', (event) => {
-        let confirm_delete = window.confirm('Are you sure you want to DELETE this group?');
+  // add confirmation popup
+  deleteGroupBtn.addEventListener('click', (event) => {
+    let confirm_delete = window.confirm('Are you sure you want to DELETE this group?');
 
-        if (confirm_delete == false) {
-            event.preventDefault();
-        }
-    });
+    if (confirm_delete == false) {
+      event.preventDefault();
+    }
+  });
 });
