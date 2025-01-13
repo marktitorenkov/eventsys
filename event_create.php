79a8d536f03cb44b9baa69617e955fd725fa3283
petya8 <43464519+petya8@users.noreply.github.com>
2025-01-13 22:47:57 +0200

    Petya branch (#6)
    * database for events
    
    * event DB
    
    * event database modification and page for create event modified
    
    * Fix line endings
    
    * Had a mistake this is the corrected version
    
    * Requested changes made trigger modified and some errors removed
    
    * modifications for the things till now
    
    * added options for deleting and changing the event only admin has this rights
    
    * extract db changes
    
    * fix line endings
    
    * merge db
    
    * Fix Mixed LF
    
    * Fix registration and data.sql
    
    * Fix incorrect imports
    
    ---------
    
    Co-authored-by: Mark Titorenkov <mark.titorenkov@outlook.com>

--- event_create.php
+++ event_create.php
@@ -1,7 +1,47 @@
 <?php
 require_once 'includes/session.php';
+require_once 'includes/db_events.php';
 
 ensureLoggedIn();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $date=$_POST['date'];
+  $description = $_POST['description'];
+  $recurring = isset($_POST['recurring']) ? true : false;
+
+  $error_messages = array();
+
+  if (empty($name)) {
+    $name = $user['username'].'s event';
+  } elseif (strlen($name) > 50) {
+    $error_messages[] = 'Event name is too long, should be less than 50 characters.';
+  }
+
+  if (!empty($group_description) && strlen($group_description) > 250) {
+    $error_messages[] = 'Description is too long, should be less than 250 characters.';
+  }
+
+
+  if (empty($error_messages)) {
+    $result = createEvent(
+      $_SESSION['user_id'],
+      true,
+      $name,
+      $date,
+      $description,
+      $recurring
+    );
+
+    if (!empty($result)) {
+      $event_id= $result;
+
+      header('Location: event_view.php?event_id=' . $event_id . '&year=' . $year);
+      exit;
+    }
+  }
+}
 ?>
 
 <?php
@@ -9,8 +49,30 @@ $page_title = "Create Event";
 include 'templates/main_header.php'
 ?>
 
+
+<!-- attaching to event will show up in next years -->
 <section class="content">
   <h1>Create Event</h1>
+  <section class="content create-event">
+    <form id="form-create-event" method="POST">
+      <input type="text" name="name" placeholder="<?php echo $user['username']?>'s event" required>
+      
+      <!-- Change input type to date -->
+      <input type="date" name="date" value="" required>
+      
+      <textarea form="form-create-event" type="text" name="description" placeholder="Description"></textarea>
+      
+      <div class="form-checkbox-wrapper">
+        <input type="checkbox" id="recurring" name="recurring">
+        <label for="recurring">Is Repeated</label>
+      </div>
+      
+      <button type="submit">Create</button>
+      
+      <!-- Include form error template if exists -->
+      <?php include 'templates/form_error.php'; ?>
+    </form>
+  </section>
 </section>
 
 <?php
