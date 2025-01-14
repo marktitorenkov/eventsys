37bb3ce29ef3fa62b10d029b47418af14d64dc7f
Mark Titorenkov <mark.titorenkov@outlook.com>
2025-01-14 19:16:55 +0200

    Make config externally configurable

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
 
 $config = [
-  'db_host' => 'localhost',
+  'db_host' => $_ENV["DB_HOST"] ?? 'localhost',
+  'db_user' => $_ENV["DB_USER"] ?? 'root',
+  'db_pass' => $_ENV["DB_PASS"] ?? '',
   'db_name' => 'eventsys',
-  'db_user' => 'root',
-  'db_pass' => '',
+
   'group_pass_characters' => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
 ];
 
